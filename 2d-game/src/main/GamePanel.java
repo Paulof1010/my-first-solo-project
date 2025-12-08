c6643cc52dc49fdcb4f77f510af3703ac68ee780
Paulof-pc <paulof-pc@codecadet.Home>
2025-12-08 15:17:56 +0000

    Created game Window using JFrame, keyboard binding & game loop using 'Delta/Accumulator' method

---
+++ 2d-game/src/main/GamePanel.java
@@ -0,0 +1,147 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable{
+
+    // SCREEN SETTINGS
+
+    final int originalTileSize = 16; // 16x16
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale; // 16x3 = 48, so our screen will be 48x48 pixels
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+    final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+
+    //FPS
+    int fps = 60;
+
+    public GamePanel() {
+
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyHandler);
+        this.setFocusable(true);
+    }
+
+    KeyHandler keyHandler = new KeyHandler();
+    Thread gameThread; // Starts our game 'clock', we need to implement 'Runnable' in order to use the Thread class
+
+    // set player default positions
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
+    public void startGameThread() {
+
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        // There are two popular game loops: 'Sleep' & 'Delta/Accumulator'. I'll use Delta because it's more accurate.
+        // But I'll write the code for the Sleep method too, for learning reasons and leave it commented
+
+                            // DELTA METHOD - Based on System time
+
+        double drawInterval = (double) 1000000000 /fps;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        // Display FPS
+        long timer = 0;
+        int drawCount = 0;
+
+        while (gameThread != null) {
+
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+
+            timer += (currentTime - lastTime); // Display FPS
+
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta --;
+                drawCount++; // Display FPS
+            }
+
+            if (timer >= 1000000000) {
+                System.out.println("FPS: " + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+        }
+
+                            // SLEEP METHOD - Based on Thread.sleep
+
+//        double drawInterval = (double) 1000000000 /fps; // 0.01666s
+//        double nextDrawTime = System.nanoTime() + drawInterval;
+//
+//        while (gameThread != null) {
+//
+//            long currentTime = System.nanoTime(); // More accurate than milliseconds
+//
+//            // Because I'll be running in 60FPS, these two methods will be called 60x/second
+//
+//            // 1 UPDATE: update info such as character positions
+//
+//            update();
+//
+//
+//            // 2 DRAW: draw the screen with the updated info
+//
+//            repaint();
+//
+//            try {
+//                double remainingTime = nextDrawTime - System.nanoTime();
+//                remainingTime = remainingTime/1000000;
+//
+//                if (remainingTime < 0) {
+//                    remainingTime = 0;
+//                }
+//
+//                Thread.sleep((long) remainingTime);
+//
+//                nextDrawTime += drawInterval;
+//
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+    }
+
+    public void update() {
+
+        if (keyHandler.upPressed) {
+            playerY -= playerSpeed;
+        } else if (keyHandler.downPressed) {
+            playerY += playerSpeed;
+        } else if (keyHandler.leftPressed) {
+            playerX -= playerSpeed;
+        } else if (keyHandler.rightPressed) {
+            playerX += playerSpeed;
+        }
+    }
+
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g); // g is our 'paint brush'
+
+        Graphics2D g2 = (Graphics2D)g; // For more functionality
+
+        g2.setColor(Color.white);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
+
+        g2.dispose();
+    }
+}
