4e0813d8e28fdcfac0cf036cc45984f7658d0112
Paulo Ferreira <p.ferreira@hotmail.com>
2025-12-09 10:10:37 +0000

    Changed Main class method signature

--- 2d-game/src/main/Main.java
+++ 2d-game/src/main/Main.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public class Main {
 
-    static void main() {
+    public static void main(String[] args) {
 
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
